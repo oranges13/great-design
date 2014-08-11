c65fb29cc1902afca2e588f34099d6c3e1c5d476
Elanor <ematheson@davenport.edu>
2014-08-11 16:22:10 -0400

    MOnday

--- js/app.js
+++ js/app.js
@@ -16,17 +16,37 @@ $('#quals').waypoint(function() {
 }, {offset: '90%'});
 
 $('#quals').waypoint(function() {
-	$('#rocket').animate({
-		opacity: 1
-	}, 700);
-	setTimeout(function() {
-		$("#term").animate({
+	$('#quals div').each(function(i) {
+		$(this).delay(200*i).animate({
 			opacity: 1
-		}, 700)
-	}, 300);
-	setTimeout(function() {
-		$("#degree").animate({
-			opacity: 1
-		}, 700)
-	}, 600);
+		}, 700);
+	})
 }, {offset: '50%'});
+
+$('#program').waypoint(function() {
+	// Reset Opacity
+	$('#program i').css("opacity", 0);
+}, {offset: '90%'});
+
+$('#program').waypoint(function() {
+	$('#program i').each(function(i) {
+		$(this).delay(200*i).animate({
+			opacity: 1
+		}, 700);
+	})
+}, {offset: '50%'});
+
+// Paralaxx Effect
+
+$window = $(window);
+ 
+$('section[data-type="background"]').each(function(){
+ var $scroll = $(this);
+                 
+  $(window).scroll(function() {                     
+    var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+    
+    var coords = '0 '+ yPos + 'px';
+    $scroll.css({ backgroundPosition: coords });    
+  });
+});
